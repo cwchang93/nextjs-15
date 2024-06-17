b821f3ed3cd6656f10388454b3dfe9aa30ef9de2
Jinwei <cwchang93@gmail.com>
2024-06-17 22:14:24 +0800

    Add components for demo

---
+++ src/app/react_compiler/ChildComponent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useEffect } from "react";
+
+const ChildComponent = ({ name }: { name: string }) => {
+  useEffect(() => {
+    console.log("in child");
+  });
+
+  return (
+    <div>
+      <div>Child Component</div>
+      Name: {name}
+    </div>
+  );
+};
+
+// export default React.memo(ChildComponent);
+export default ChildComponent;
---
+++ src/app/react_compiler/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import ChildComponent from "./ChildComponent";
+
+const ReactCompilerPage = () => {
+  const [count, setCount] = useState(0);
+
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    console.log("ReactCompilerPage");
+  });
+
+  return (
+    <div className="w-screen h-screen">
+      <div>React Compiler</div>
+      <button
+        className="border border-black"
+        onClick={() => setCount((c) => c + 1)}
+      >
+        Add Count
+      </button>
+      <div>Count: {count}</div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <ChildComponent name={name} />
+    </div>
+  );
+};
+
+export default ReactCompilerPage;
